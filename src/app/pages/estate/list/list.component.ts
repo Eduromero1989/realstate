6228885767eeb2ba02dc9ea6d194c048bbe9522b
AngeyVelez <angeyvelez@gmail.com>
2023-05-16 18:15:05 -0500

    Listar inmuebles

---
+++ src/app/pages/estate/list/list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { IEstate } from '../estate.interface';
+import { EstateData } from '../../../@core/data/estate';
+
+@Component({
+  selector: 'ngx-list',
+  templateUrl: './list.component.html',
+  styleUrls: ['./list.component.scss']
+})
+export class ListComponent implements OnInit {
+
+  searchValue: string = '';
+
+  estates: IEstate[] = []
+
+  constructor(private estateService: EstateData) { }
+
+  ngOnInit(): void {
+    this.estateService.getEstates().subscribe((estates: IEstate[]) => {
+      this.estates = estates;
+    });
+  }
+
+}
