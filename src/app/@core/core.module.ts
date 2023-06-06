0da956819d07cce731ed68d357e73761707c87f1
AngeyVelez <angeyvelez@gmail.com>
2023-06-06 18:40:17 -0500

    Fix environment

--- src/app/@core/core.module.ts
+++ src/app/@core/core.module.ts
@@ -55,7 +55,7 @@ import { MockDataModule } from './mock/mock-data.module';
 import { EstateData } from './data/estate';
 import { BranchOfficeData } from './data/branch-office';
 import { BranchOfficeService } from './mock/branch-office';
-import { EstateService } from './services/estate.service';
+import { EstateService } from './mock/estate.service';
 
 const socialLinks = [
   {
