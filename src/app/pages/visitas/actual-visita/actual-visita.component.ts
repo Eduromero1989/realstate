b4b6faad5a44c435e4ab434569a15bd6ec8cbfaa
@devjunior7titanq <devjunior7titanq@gmail.com>
2023-06-08 21:15:13 -0500

    avances listar-actualizar visita

--- src/app/pages/visitas/actual-visita/actual-visita.component.ts
+++ src/app/pages/visitas/actual-visita/actual-visita.component.ts
@@ -16,6 +16,9 @@ import { ServicesVisitaService } from '../../../services-visita.service';
 export class ActualVisitaComponent implements OnInit {
 
   data: any;
+  datos: any;
+  json_convertido: any;
+  listaInmuebles: [] = [];
 
   username = 'calosegc11@gmail.com';
   password = '12345';
@@ -31,36 +34,36 @@ export class ActualVisitaComponent implements OnInit {
 
 
 
-  listaInmuebles = [
-    {
-      "fechaSolicitud": "15/05/2023",
-      "fechaVisita": "22/05/2023",
-      "Hora": "05:23 pm",
-      "Referencia": "0000",
-      "comentario": "Gran lugar"
-    },
-    {
-      "fechaSolicitud": "16/05/2023",
-      "fechaVisita": "23/05/2023",
-      "Hora": "04:23 pm",
-      "Referencia": "1111",
-      "comentario": "Magnífico"
-    },
-    {
-      "fechaSolicitud": "17/05/2023",
-      "fechaVisita": "24/05/2023",
-      "Hora": "02:23 pm",
-      "Referencia": "2222",
-      "comentario": "Estupenda vista"
-    },
-    {
-      "fechaSolicitud": "18/05/2023",
-      "fechaVisita": "25/05/2023",
-      "Hora": "03:23 pm",
-      "Referencia": "3333",
-      "comentario": "Mala ubicación"
-    },
-  ];
+  // listaInmuebles = [
+  //   {
+  //     "fechaSolicitud": "15/05/2023",
+  //     "fechaVisita": "22/05/2023",
+  //     "Hora": "05:23 pm",
+  //     "Referencia": "0000",
+  //     "comentario": "Gran lugar"
+  //   },
+  //   {
+  //     "fechaSolicitud": "16/05/2023",
+  //     "fechaVisita": "23/05/2023",
+  //     "Hora": "04:23 pm",
+  //     "Referencia": "1111",
+  //     "comentario": "Magnífico"
+  //   },
+  //   {
+  //     "fechaSolicitud": "17/05/2023",
+  //     "fechaVisita": "24/05/2023",
+  //     "Hora": "02:23 pm",
+  //     "Referencia": "2222",
+  //     "comentario": "Estupenda vista"
+  //   },
+  //   {
+  //     "fechaSolicitud": "18/05/2023",
+  //     "fechaVisita": "25/05/2023",
+  //     "Hora": "03:23 pm",
+  //     "Referencia": "3333",
+  //     "comentario": "Mala ubicación"
+  //   },
+  // ];
 
   constructor(
 
@@ -72,16 +75,19 @@ export class ActualVisitaComponent implements OnInit {
 
   ngOnInit(): void {
     this.showInfo();
+
     this.visitasService
       .service('http://localhost:5235/ServicioVisitas', 'get')
       .subscribe(
         (response) => {
-          console.log('Response ApiService:', response);
-          // Hacer algo con la respuesta del servicio
+          console.log('Respuesta:', response.datos[0].comentario);
+          this.listaInmuebles = response.datos;
+          console.log('json convertido: ', this.listaInmuebles);
+          
         },
         (error) => {
           console.error('Error ApiService:', error);
-          // Manejar el error de la solicitud
+          
         }
       );
   }
@@ -98,6 +104,11 @@ export class ActualVisitaComponent implements OnInit {
     // console.log(this.listaInmuebles);
   }
 
+  insertComment(): void{
+    this.visitasService
+    .service('http://localhost:5235/ServicioVisitas', 'put', )
+    .subscribe();
+  }
   /**
  * @description función que abre modal para agregar un comentario
  * @param
@@ -106,6 +117,14 @@ export class ActualVisitaComponent implements OnInit {
  * @returns
  */
   open(): void {
+    this.datos = {
+      "id": 1,
+      "idOferta": 1,
+      "idPersona": 2,
+      "fecha": "2023-06-09T01:43:09.165Z",
+      "realizada": true,
+      "comentario": "Nuevo comentario"
+    };
     this.dialogService.open(DialogCommentComponent, {
       context: {
         title: 'Ingresa tus comentarios',
